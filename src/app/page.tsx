87ace8d255a24d5539b95a0266b4dcbbe7612f3a
greasycat <131315c@gmail.com>
2025-03-03 16:46:21 -0500

    remove google oauth for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 import Dashboard from "./components/dashboard";
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 export default function Home() {
   return (
-    <GoogleOAuthProvider clientId="451551445631-muf463qq03bf7s6h6hf7u51hvlh8l64t.apps.googleusercontent.com">
-      <Dashboard />
-    </GoogleOAuthProvider>
+    <Dashboard />
   )
 }
 
